2295ff8c94f051ed2518a80799a582e89af1a7bf
Tim Gage <tsg21@users.noreply.github.com>
2025-01-14 19:03:51 +0000

    Works with full code now

--- src/patterns/twinklesMod.cpp
+++ src/patterns/twinklesMod.cpp
@@ -9,15 +9,62 @@
 class Twinkle2 : public MultiPatternItem {
  private:
   int pos;
+  CRGB color;
+  int seq;
+  int scaleUpRate;
+  int scaleDownRate;
+  int scaleDownFromSeq;
+  int deathSeq;
+  bool isRainbow;
 
  public:
-  Twinkle2(int randValue) { pos = randValue % NUM_LEDS; }
+  Twinkle2(int randValue) {
+    seq = 0;
 
-  void update() override {}
+    pos = randValue % NUM_LEDS;
 
-  bool isDead() override { return false; }
+    int colorChoice = randValue % 1000;
 
-  void render(CRGB* leds) override { leds[pos].setRGB(255, 0, 0); }
+    isRainbow = false;
+    if (colorChoice < 800) {
+      color = CRGB(255, 255, 255);  // white
+      scaleUpRate = 5;
+      scaleDownRate = 5;
+    } else if (colorChoice < 970) {
+      color = CRGB(245, 239, 66);  // gold
+      scaleUpRate = 2;
+      scaleDownRate = 2;
+    } else {
+      isRainbow = true;
+      deathSeq = 220;
+    }
+
+    if (!isRainbow) {
+      scaleDownFromSeq = 255 / scaleUpRate;
+      deathSeq =
+          scaleDownFromSeq + (scaleUpRate * scaleDownFromSeq) / scaleDownRate;
+    }
+  }
+
+  void update() override { seq++; }
+
+  bool isDead() override { return seq >= deathSeq; }
+
+  void render(CRGB* leds) override {
+    if (isRainbow) {
+      uint8_t hue = seq;
+      leds[pos].setHSV(hue, 255, 255);
+    } else {
+      int fadeFactor = seq < scaleDownFromSeq
+                           ? seq * scaleUpRate
+                           : (scaleDownFromSeq * scaleUpRate) -
+                                 ((seq - scaleDownFromSeq) * scaleDownRate);
+
+      if (fadeFactor > 0) {
+        leds[pos] = color.scale8(fadeFactor);
+      }
+    }
+  }
 };
 
 class TwinklesMod::Impl {
